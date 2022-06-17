0e91e9458141978c877e7aeb82f97ae914f381e7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-17 21:27:03 +0900

    :memo: #14.5 | Create Account part One
    - create account layout

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -9,6 +9,7 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   background-color: black;
+  padding: 0px 40px;
 `;
 
 const Logo = styled.Image`
@@ -16,21 +17,23 @@ const Logo = styled.Image`
   height: 100px;
 `;
 
-const CreateAccount = styled.View`
+const CreateAccount = styled.TouchableOpacity`
   background-color: ${colors.blue};
-  padding: 7px 10px;
+  padding: 10px 10px;
   border-radius: 5px;
+  width: 100%;
 `;
 
 const CreateAccountText = styled.Text`
   color: white;
   font-weight: 600;
+  text-align: center;
 `;
 
 const LoginLink = styled.Text`
   color: ${colors.blue};
   font-weight: 600;
-  margin-top: 10px;
+  margin-top: 20px;
 `;
 
 const Welcome = ({
@@ -41,11 +44,9 @@ const Welcome = ({
   return (
     <Container>
       <Logo resizeMode="contain" source={require("../assets/logo.png")} />
-      <TouchableOpacity onPress={goToCreateAccount}>
-        <CreateAccount>
-          <CreateAccountText>Create Account</CreateAccountText>
-        </CreateAccount>
-      </TouchableOpacity>
+      <CreateAccount onPress={goToCreateAccount}>
+        <CreateAccountText>Create Account</CreateAccountText>
+      </CreateAccount>
       <TouchableOpacity onPress={goToLogin}>
         <LoginLink>Login</LoginLink>
       </TouchableOpacity>
