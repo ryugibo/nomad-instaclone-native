ddc4ab2fdd9c1732c28e0473febcb6ee33334d0c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-18 02:25:18 +0900

    :memo: #15.0 | Tab Navigator

--- navigators/LoggedInNav.tsx
+++ navigators/LoggedInNav.tsx
@@ -1,12 +1,84 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Feed from "../screens/Feed";
+import Notifications from "../screens/Notifications";
+import Profile from "../screens/Profile";
+import Search from "../screens/Search";
 
 const Tabs = createBottomTabNavigator();
 
 const LoggedInNav = () => {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name="home"
+                color={color}
+                size={size * (focused ? 1.3 : 1.0)}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name="search"
+                color={color}
+                size={size * (focused ? 1.3 : 1.0)}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name="heart-outline"
+                color={color}
+                size={size * (focused ? 1.3 : 1.0)}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name="person"
+                color={color}
+                size={size * (focused ? 1.3 : 1.0)}
+              />
+            );
+          },
+        }}
+      />
     </Tabs.Navigator>
   );
 };
