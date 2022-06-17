c1f3561b71c2e7cf4ab947bf3f7e98b93ba43248
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-17 21:28:20 +0900

    :memo: #14.12 | Log In Mutation

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,18 +1,45 @@
+import { gql, useMutation } from "@apollo/client";
 import { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { TextInput } from "react-native";
+import { isLoggedInVar } from "../apollo";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 import { FormTextInput } from "../components/auth/AuthShared";
 
+const LOG_IN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(userName: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
 const Login = () => {
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, handleSubmit, setValue, watch } = useForm();
   const passwordRef = useRef<TextInput>(null);
+  const onCompleted = (data: any) => {
+    const {
+      login: { ok, token },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    isLoggedInVar(true);
+  };
+  const [logInMutation, { loading }] = useMutation(LOG_IN_MUTATION, {
+    onCompleted,
+  });
   const onNext = (nextOne: React.RefObject<TextInput>) => {
     nextOne.current?.focus();
   };
   const onValid = (data: any) => {
-    console.log(data);
+    if (loading) {
+      return;
+    }
+    logInMutation({ variables: { ...data } });
   };
   useEffect(() => {
     register("username", { required: true });
@@ -38,7 +65,12 @@ const Login = () => {
         onSubmitEditing={handleSubmit(onValid)}
         onChangeText={(text) => setValue("password", text)}
       />
-      <AuthButton text="Log in" onPress={handleSubmit(onValid)} />
+      <AuthButton
+        text="Log in"
+        loading={loading}
+        disabled={!watch("username") || !watch("password")}
+        onPress={handleSubmit(onValid)}
+      />
     </AuthLayout>
   );
 };
