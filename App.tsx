f2ddd1ffc1663dc0ce9fe394fece00acae47380c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-17 12:22:35 +0900

    :memo: #13.6 | AppLoading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
+    console.log(fontPromises);
+    await Promise.all(fontPromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
